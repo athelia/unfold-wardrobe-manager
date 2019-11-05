08736ac7e7ba34878eeba1ff6e144a77ddde91d9
Athelia Crosmun <athelia.hk@gmail.com>
2019-11-05 12:12:24 -0800

    Add seed data; complete model.py; work on seed.py

--- model.py
+++ model.py
@@ -5,6 +5,9 @@ from sqlalchemy import asc, update
 
 db = SQLAlchemy()
 
+##############################################################################
+# Model definitions
+
 class User(db.Model):
     """User of wardrobe manager website."""
 
@@ -18,12 +21,12 @@ class User(db.Model):
         return f"<User ID={self.user_id} Email={self.email}>"
 
 
-class Category(db.Model):
-    """Standard category of clothing articles."""
+class BaseCategory(db.Model):
+    """Standard categories of clothing articles."""
 
-    __tablename__ = "categories"
+    __tablename__ = "base_categories"
 
-    category_id = db.Column(db.String(10), primary_key=True)
+    base_category_id = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     description = db.Column(db.String(256), nullable=True)
 
@@ -38,12 +41,12 @@ class UserCategory(db.Model):
 
     user_category_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    category_id = db.Column(db.String(10), db.ForeignKey('categories.category_id'), nullable=False)
+    base_category_id = db.Column(db.String(10), db.ForeignKey('base_categories.base_category_id'), nullable=False)
     name = db.Column(db.String(64), nullable=False)
     description = db.Column(db.String(256), nullable=True)
 
     # Define relationship to Category
-    category = db.relationship("Category", backref=db.backref("users_categories"))
+    base_category = db.relationship('BaseCategory', backref=db.backref('users_categories'))
 
     def __repr__(self):
         return f"<UserCategory ID={self.user_category_id} Name={self.name}>"
@@ -60,17 +63,15 @@ class Article(db.Model):
     description = db.Column(db.String(256), nullable=True)
     image = db.Column(db.String(), nullable=True)
     purchase_price = db.Column(db.Float, nullable=True)
-    times_worn = db.Column(db.Integer, default=int(0), nullable=False)
+    times_worn = db.Column(db.Integer, default=0, nullable=False)
     sell_price = db.Column(db.Float, nullable=True)
 
     # Define relationship to UserCategory
-    user_category = db.relationship("UserCategory", backref=db.backref("articles"))
-
-    # Define relationship to Outfit
+    user_category = db.relationship('UserCategory', backref=db.backref('articles'))
 
 
     # Define relationship to TagArticle
-
+    tag_article = db.relationship('TagArticle', backref=db.backref('articles'))
 
     def __repr__(self):
         return f"<Article ID={self.article_id} Category={self.user_category.name} Description={self.description:.15}>"
@@ -82,13 +83,32 @@ class Outfit(db.Model):
     __tablename__ = "outfits"
 
     outfit_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    name = db.Column(db.String(64), nullable=True)
+    description = db.Column(db.String(256), nullable=True)
+    times_worn = db.Column(db.Integer, default='0', nullable=False)
 
-
-    #Define relationship to TagOutfit
-
-    
     def __repr__(self):
-        pass
+        return f"<Outfit ID={self.outfit_id} Name={self.name} Description={self.description:.15}>"
+
+
+class ArticleOutfit(db.Model):
+    """Association table for articles and outfits."""
+
+    __tablename__ = "articles_outfits"
+
+    article_outfit_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    article_id = db.Column(db.Integer, db.ForeignKey('articles.article_id'), nullable=False)
+    outfit_id = db.Column(db.Integer, db.ForeignKey('outfits.outfit_id'), nullable=False)
+
+    # Define relationship to Article
+    article = db.relationship('Article', backref=db.backref('articles_outfits'))
+
+    # Define relationship to Outfit
+    outfit = db.relationship('Outfit', backref=db.backref('articles_outfits'))
+
+    def __repr__(self):
+        return f"<ArticleOutfit ID={self.article_outfit_id} Article ID={self.article_id} Outfit ID={self.outfit_id}>"
 
 
 class Tag(db.Model):
@@ -97,9 +117,11 @@ class Tag(db.Model):
     __tablename__ = "tags"
 
     tag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    name = db.Column(db.String(32), nullable=False)
 
     def __repr__(self):
-        pass
+        return f"<Tag ID={self.tag_id} Name={self.name}>"
 
 
 class TagArticle(db.Model):
@@ -108,9 +130,17 @@ class TagArticle(db.Model):
     __tablename__ = "tags_articles"
 
     tag_article_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    article_id = db.Column(db.Integer, db.ForeignKey('articles.article_id'), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.tag_id'), nullable=False)
+
+    # Define relationship to Tag
+    tag = db.relationship('Tag', backref=db.backref('tags_articles'))
+
+    # Define relationship to Article
+    article = db.relationship('Article', backref=db.backref('tags_articles'))
 
     def __repr__(self):
-        pass
+        return f"<TagArticle ID={self.tag_article_id} Tag ID={self.tag_id} Article ID={self.article_id}>"
 
 
 class TagOutfit(db.Model):
@@ -119,6 +149,37 @@ class TagOutfit(db.Model):
     __tablename__ = "tags_outfits"
 
     tag_outfit_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    outfit_id = db.Column(db.Integer, db.ForeignKey('outfits.outfit_id'), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.tag_id'), nullable=False)
+
+    # Define relationship to Tag
+    tag = db.relationship('Tag', backref=db.backref('tags_outfits'))
+
+    # Define relationship to Outfit
+    outfit = db.relationship('Outfit', backref=db.backref('tags_outfits'))
 
     def __repr__(self):
-        pass
+        return f"<TagOutfit ID={self.tag_outfit_id} Tag ID={self.tag_id} Outfit ID={self.outfit_id}>"
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///clothes'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app 
+    connect_to_db(app)
+    print("Connected to DB.")
