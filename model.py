2836a13e6dddb89713d9dfa40b7b1cdf7bfe06d5
Athelia Crosmun <athelia.hk@gmail.com>
2019-12-07 17:53:30 -0800

    Comment out debug mode for screencast

--- model.py
+++ model.py
@@ -958,7 +958,7 @@ def connect_to_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///clothes'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///testclothes'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ECHO'] = True
+    # app.config['SQLALCHEMY_ECHO'] = True
     db.app = app
     db.init_app(app)
 
--- server.py
+++ server.py
@@ -847,13 +847,12 @@ def test_weather_darksky():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
-    # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
+    # app.debug = True
+    # # make sure templates, etc. are not cached in debug mode
+    # app.jinja_env.auto_reload = app.debug
 
+    # # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+    
     connect_to_db(app)
-
-    # Use the DebugToolbar
-    DebugToolbarExtension(app)
-
     app.run(port=5000, host='0.0.0.0')
